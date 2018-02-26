3b4b76f7f18f5487209139cae645c88e42a3e039
AparnaAppus <sp.aparna765@gmail.com>
2018-02-26 15:17:12 +0530

    added

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var http = require('http')
 http.createServer(function (request,response){
   response.writeHead(200, {"Content-Type": "text/plain"})
   response.end("Hello World\n")
-}).listen(process.env.PORT)
+}).listen(process.env.PORT||8000)
