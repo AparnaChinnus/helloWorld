18564c08b9f06fd6c7c29c87a42e98a846211bd7
AparnaAppus <sp.aparna765@gmail.com>
2018-02-26 16:56:54 +0530

    added web page

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ var app = express();
 var path = require('path');
 var bodyParser = require("body-parser");
 var port=process.env.PORT || 7000;
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/web/app.html'));
+});
 app.set('views', path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname, 'web')));
-app.get('/', function(req, res) {
-  window.navigate("app.html");
-    });
-app.get('/asset', function (req, res){
+// app.get('/asset', function (req, res){
+//
+//   });
 
 app.listen(port);
 
